d05527209251cb654a0b0ce9a0fe366cb0ca51f5
aaronpangan <73965654+aaronpangan@users.noreply.github.com>
2022-12-15 02:22:15 +0800

    Add isLoading state in navItems

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,18 @@
 import { useSupabaseClient, useUser } from '@supabase/auth-helpers-react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import styles from '../styles/Header.module.css';
 // import Search from './Search';
 const Header = () => {
-  const user = useUser();
   const supabaseClient = useSupabaseClient();
   const router = useRouter();
+  const user = useUser();
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, [user]);
 
   const handleSignOut = async () => {
     const { error } = await supabaseClient.auth.signOut();
@@ -16,42 +22,45 @@ const Header = () => {
     }
   };
 
+  const RenderNavItems = () => {
+    if (!user) {
+      return (
+        <>
+          <li>
+            <Link href="/concert">Concerts</Link>
+          </li>
+
+          <li>
+            <Link href="/login">Sign In</Link>
+          </li>
+          <li>
+            <Link href="/register">Sign Up</Link>
+          </li>
+        </>
+      );
+    } else if (user)
+      return (
+        <>
+          <li>
+            <Link href="/dashboard">Dashboard</Link>
+          </li>
+
+          <li>
+            <Link href="/concert/add">Add Concert</Link>
+          </li>
+
+          <li onClick={handleSignOut}>Sign Out</li>
+        </>
+      );
+  };
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
         <Link href="/">DJ Events</Link>
       </div>
-      {/* <Search /> */}
 
       <nav>
-        <ul>
-          {!user ? (
-            <>
-              <li>
-                <Link href="/concert">Concerts</Link>
-              </li>
-
-              <li>
-                <Link href="/login">Sign In</Link>
-              </li>
-              <li>
-                <Link href="/register">Sign Up</Link>
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <Link href="/dashboard">Dashboard</Link>
-              </li>
-
-              <li>
-                <Link href="/concert/add">Add Concert</Link>
-              </li>
-
-              <li onClick={handleSignOut}>Sign Out</li>
-            </>
-          )}
-        </ul>
+        <ul>{!isLoading ? <RenderNavItems /> : <></>}</ul>
       </nav>
     </header>
   );
